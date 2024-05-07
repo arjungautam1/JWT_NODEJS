1a04738c52d12798136ad7c0953f08071b83eabb
arjungautam1 <arjungautam5431@gmail.com>
2024-05-07 18:52:17 -0400

    bearer level authentication completed

--- server.js
+++ server.js
@@ -37,9 +37,11 @@ function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (token == null) return res.sendStatus(401);
+  if (token == null) {
+    return res.status(401).json({ message: "Unauthorized User" });
+  }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) return res.status(403).json({ message: "Authorization Failed." });
     req.user = user;
     next();
   });
